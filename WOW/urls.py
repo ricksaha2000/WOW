b846e0e167f8a819580bb824693a8c1fd6312d87
Aritra Mondal <1806290@kiit.ac.in>
2019-11-02 11:03:50 +0530

    Added Accounts App

--- WOW/urls.py
+++ WOW/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth.views import LogoutView
 
 from home.views import home_view, about_view, contact_view
+from accounts.views import login_page, register_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name="home"),
     path('about/', about_view, name="about"),
     path('contact/', contact_view, name="contact"),
-
+    path('login/', login_page, name="login"),
+    path('logout', LogoutView.as_view(), name="logout"),
+    path('registration/', register_page, name="register"),
 ]
-urlpatterns +=  staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns()
--- home/views.py
+++ home/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 
+
 def home_view(request):
     context = {
         'title': 'Home',
     }
     return render(request, 'home/home.html', context)
 
+
 def about_view(request):
     context = {
         'title': 'About Us',
     }
     return render(request, 'home/about.html', context)
+
 
 def contact_view(request):
     context = {
