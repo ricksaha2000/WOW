6d4cebf67169d7c0ae5e2593339ef5e0118ec398
ricksaha2000 <45602643+ricksaha2000@users.noreply.github.com>
2019-12-11 16:51:07 +0530

    Initial Commit

---
+++ js/script3.js
@@ -0,0 +1,126 @@
+$('.toggle').click(function(){
+    
+    $('#target').toggle('slow');
+    $('#target1').hide(0);
+    $('#target2').hide(0);
+    $('#target3').hide(0);
+    $('#target4').hide(0);
+    $('#target5').hide(0);
+    $('#target6').hide(0);
+    $('#target7').hide(0);
+    $('#target8').hide(0);
+
+
+
+    
+    
+    
+});
+$('.toggle1').click(function(){
+    
+    $('#target1').toggle('slow');
+    $('#target').hide(0);
+    $('#target2').hide(0);
+    
+    
+    
+});
+$('.toggle2').click(function(){
+    
+    $('#target2').toggle('slow');
+    $('#target1').hide(0);
+    $('#target').hide(0);
+    
+    
+    
+});
+$('.toggle3').click(function(){
+    
+    $('#target3').toggle('slow');
+    $('#target1').hide(0);
+    $('#target2').hide(0);
+    $('#target').hide(0);
+    $('#target4').hide(0);
+    $('#target5').hide(0);
+    $('#target6').hide(0);
+    $('#target7').hide(0);
+    $('#target8').hide(0);
+
+});
+
+
+$('.toggle4').click(function(){
+    
+    $('#target4').toggle('slow');
+    $('#target1').hide(0);
+    $('#target2').hide(0);
+    $('#target3').hide(0);
+    $('#target').hide(0);
+    $('#target5').hide(0);
+    $('#target6').hide(0);
+    $('#target7').hide(0);
+    $('#target8').hide(0);
+
+});
+
+$('.toggle5').click(function(){
+    
+    $('#target5').toggle('slow');
+    $('#target1').hide(0);
+    $('#target2').hide(0);
+    $('#target').hide(0);
+    $('#target4').hide(0);
+    $('#target').hide(0);
+    $('#target6').hide(0);
+    $('#target7').hide(0);
+    $('#target8').hide(0);
+
+});
+
+$('.toggle6').click(function(){
+    
+    $('#target6').toggle('slow');
+    $('#target1').hide(0);
+    $('#target2').hide(0);
+    $('#target').hide(0);
+    $('#target4').hide(0);
+    $('#target5').hide(0);
+    $('#target').hide(0);
+    $('#target7').hide(0);
+    $('#target8').hide(0);
+
+});
+
+$('.toggle7').click(function(){
+    
+    $('#target7').toggle('slow');
+    $('#target1').hide(0);
+    $('#target2').hide(0);
+    $('#target').hide(0);
+    $('#target4').hide(0);
+    $('#target5').hide(0);
+    $('#target6').hide(0);
+    $('#target').hide(0);
+    $('#target8').hide(0);
+
+});
+
+$('.toggle8').click(function(){
+    
+    $('#target8').toggle('slow');
+    $('#target1').hide(0);
+    $('#target2').hide(0);
+    $('#target').hide(0);
+    $('#target4').hide(0);
+    $('#target5').hide(0);
+    $('#target6').hide(0);
+    $('#target7').hide(0);
+    $('#target').hide(0);
+
+});
+
+
+
+
+
+
